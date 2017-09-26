794329075ed91bc8ccd19ebc3b9ffba1ab655a72
OhmG-999 <mgoardet@hotmail.fr>
2017-09-27 00:01:38 +0100

    added comment
    added comment to excersice 5

--- exercise5.py
+++ exercise5.py
@@ -1,9 +1,11 @@
 # create variables to hold the user input
 myword1 = input('Enter the first word: \n')
 myword2 = input('Enter the second word: \n')
+
+# revert the word starting by the last character
 reversedword = myword1[::-1]
 
-
+# very if reverse word is the same as word2
 if reversedword == myword2:
     print(myword1,'is the inverse of',myword2)
 else:
